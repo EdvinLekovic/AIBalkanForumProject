39568b3e44ce875cb36ff60f1b46fa04e5c6bd0e
EdvinLekovic <edvinleko12@gmail.com>
2021-02-12 15:24:37 +0100

    adding

---
+++ src/main/java/com/webproject/aibalkanforumproject/AiBalkanForumProjectApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.aibalkanforumproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBalkanForumProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiBalkanForumProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webproject/aibalkanforumproject/AiBalkanForumProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.aibalkanforumproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBalkanForumProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
