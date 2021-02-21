860fe1cd3f2475e5cfe8d936ce43c7c8eebce6b4
stavrovfilip <filip.stavrov@students.finki.ukim.mk>
2021-02-21 01:44:14 +0100

    Add research form, overview of a research and general changes in design

--- src/main/java/com/webproject/aibalkanforumproject/web/ResearchesController.java
+++ src/main/java/com/webproject/aibalkanforumproject/web/ResearchesController.java
@@ -1,13 +1,24 @@
 package com.webproject.aibalkanforumproject.web;
 
+import com.webproject.aibalkanforumproject.model.Category;
+import com.webproject.aibalkanforumproject.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/researches")
 public class ResearchesController {
+
+    private final CategoryService categoryService;
+
+    public ResearchesController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
 
     @GetMapping
     public String getResearchesPage(Model model){
@@ -15,9 +26,18 @@ public class ResearchesController {
         return "master-template";
     }
 
+    @GetMapping("/add-form")
+    public String getAddProductPage(Model model){
+        List<Category> categories = this.categoryService.listCategories();
+        model.addAttribute("categories", categories);
+        model.addAttribute("bodyContent", "addResearch");
 
-//    @GetMapping
-//    public String getResearchRead(Model model){
-//    return "";
-//    }
+        return "master-template";
+    }
+
+    @GetMapping("/id")
+    public String getResearchRead(Model model){
+        model.addAttribute("bodyContent","article");
+        return "master-template";
+    }
 }
